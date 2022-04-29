d046d3871aa56bf1ec87d077ccb71c8f236ccdfc
Yaroslav131 <berserk12186@gmail.com>
2022-04-29 23:06:27 +0300

    Add console provider

---
+++ Your_Money/Your_Money/Providers/ConsoleProvider.cs
@@ -0,0 +1,124 @@
+﻿using Your_Money.Models;
+
+namespace Your_Money.Providers
+{
+    public class ConsoleProvider
+    {
+        private User user;
+        private MathCalculation calculation;
+        private Parser parser;
+
+        public ConsoleProvider()
+        {
+            parser = new Parser();
+
+            calculation = new MathCalculation();
+
+            user = new User();
+
+            user.controls.UserControlsNotify += Display;
+
+            parser.DisplayString = Display;
+        }
+
+        private void Display(string text)
+        {
+            Console.WriteLine(text);
+
+            Thread.Sleep(2500);
+        }
+
+        private void DisplayTable<T>(List<T> entry) where T : Entry
+        {
+            if (entry.Count.Equals(0))
+            {
+                Console.WriteLine(Resurses.EmptyListExeption);
+
+                return;
+            }
+
+            for (int counter = 0; counter < entry.Count; counter++)
+            {
+                Console.Write($"{counter} {entry[counter].Score} {entry[counter].Date} \t");
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        private string? GetUserLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public void SelectOperation(int cond,
+             Func<Func<string>, double> doubleParser,
+             Func<Func<string>, int> intParser)
+        {
+            switch (cond)
+            {
+                case (int)MenuOptions.AddIncomeEntry:
+                    user.controls.AddIncomeEntry(doubleParser, GetUserLine, user.incomes);
+                    break;
+
+                case (int)MenuOptions.AddConsumptionEntry:
+                    user.controls.AddConsumptionEntry(doubleParser, GetUserLine, user.consumptions);
+                    break;
+
+                case (int)MenuOptions.DelleteIncomeEntry:
+                    user.controls.DelleteIncomeEntry(intParser, GetUserLine, user.incomes);
+                    break;
+
+                case (int)MenuOptions.DelleteConsumptionEntry:
+                    user.controls.DelleteConsumptionEntry(intParser, GetUserLine, user.consumptions);
+                    break;
+
+                case (int)MenuOptions.ChangeIncomeEntry:
+                    user.controls.ChangeIncomeEntry(intParser, doubleParser, GetUserLine, user.incomes);
+                    break;
+
+                case (int)MenuOptions.ChangeConsumptionEntry:
+                    user.controls.ChangeConsumptionEntry(intParser, doubleParser, GetUserLine, user.consumptions);
+                    break;
+
+                case (int)MenuOptions.ShowIncomesTable:
+                    DisplayTable(user.incomes);
+                    break;
+
+                case (int)MenuOptions.ShowConsumptionTable:
+                    DisplayTable(user.consumptions);
+                    break;
+
+                case (int)MenuOptions.ShowArrive:
+                    Display($"{Resurses.ArriveString} " +
+                        $"{calculation.CalculateListSum(user.incomes) - calculation.CalculateListSum(user.consumptions)}");
+                    break;
+
+                case (int)MenuOptions.Exite:
+                    break;
+
+                default:
+                    Display(Resurses.ExeprionString);
+                    break;
+            }
+        }
+
+        public void RunUserInterface()
+        {
+            int userCond = 0;
+
+            do
+            {
+                Console.Clear();
+
+                Display(Resurses.MenuString);
+
+                userCond = parser.ParseToIntId(GetUserLine);
+
+                SelectOperation(userCond, parser.ParseToDoubleSum, parser.ParseToIntId);
+            }
+            while (userCond != (int)MenuOptions.Exite);
+        }
+    }
+}
