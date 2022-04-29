4f421129338df06c5c4b9026fa7daefc655d49c7
Yaroslav131 <berserk12186@gmail.com>
2022-04-29 22:59:40 +0300

    Add resurses

---
+++ Your_Money/Your_Money/Helpes/Resurses.cs
@@ -0,0 +1,26 @@
+﻿namespace Your_Money
+{
+    public class Resurses
+    {
+        public const string MenuString =
+            "1)Add income\n" +
+            "2)Add consumption\n" +
+            "3)Remove income \n" +
+            "4)Remove consumption \n" +
+            "5)Change income \n" +
+            "6)Change consumption\n" +
+            "7)Show income entrys\n" +
+            "8)Show consumption entrys\n" +
+            "9)Сalculate Arrive\n" +
+            "10)Exite\n";
+        public const string InvateWriteString = "Write :";
+        public const string InvateWriteMoneyString = "Write sum:";
+        public const string InvateWriteIdString = "Write id:";
+        public const string ArriveString = "Arrive = ";
+        public const string ExeprionString = "Wrong input value";
+        public const string NegativeNumExeption = "Entered a negative number";
+        public const string IndexOutOfRangeExeption = "Index out of range";
+        public const string EmptyListExeption = "List is empty";
+        public const string SeccusessOpetationStr = "Operation is seccusessfull";
+    }
+}
