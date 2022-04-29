3c4167d9b9aca713a9761b425135d7c0202d2731
Yaroslav131 <berserk12186@gmail.com>
2022-04-29 18:24:06 +0300

    Add Entery model

---
+++ Your_Money/Your_Money/Models/Entry.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Your_Money
+{
+    public abstract class Entry
+    {
+        public int Id { get; set; }
+        public double Score { get; set; }
+        public DateTime Date { get; set; }
+
+        public Entry(double money, DateTime date)
+        {
+            Score = money;
+            Date = date;
+        }
+
+        public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+
+            if (Score<0)
+                errors.Add(new ValidationResult("Number under zero"));
+
+            return errors;
+        }
+    }
+}
