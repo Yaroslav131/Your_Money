4e87172bd6dc7a03fbbbe22f088b817a640673cc
Yaroslav131 <berserk12186@gmail.com>
2022-04-29 22:58:45 +0300

    Add menu enum

---
+++ Your_Money/Your_Money/Enums/Enums.cs
@@ -0,0 +1,16 @@
+﻿namespace Your_Money
+{
+    public enum MenuOptions
+    {
+        AddIncomeEntry,
+        AddConsumptionEntry,
+        DelleteIncomeEntry,
+        DelleteConsumptionEntry,
+        ChangeIncomeEntry,
+        ChangeConsumptionEntry,
+        ShowIncomesTable,
+        ShowConsumptionTable,
+        ShowArrive,
+        Exite
+    }
+}
