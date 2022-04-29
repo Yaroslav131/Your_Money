033529b76afba116d95e1dfa72f11ec1fdb8b23e
Yaroslav131 <berserk12186@gmail.com>
2022-04-29 22:59:12 +0300

    Add parsers

---
+++ Your_Money/Your_Money/Helpes/Parser.cs
@@ -0,0 +1,36 @@
+﻿namespace Your_Money
+{
+    public class Parser
+    {
+        public delegate void ParserHandler(string info);
+        public ParserHandler? DisplayString;
+
+        public int ParseToIntId(Func<string> getString)
+        {
+            int resoult;
+
+            DisplayString?.Invoke(Resurses.InvateWriteIdString);
+
+            while (!int.TryParse(getString(), out resoult))
+            {
+                DisplayString?.Invoke(Resurses.ExeprionString);
+            }
+
+            return resoult - 1;
+        }
+
+        public double ParseToDoubleSum(Func<string> getString)
+        {
+            double resoult;
+
+            DisplayString?.Invoke(Resurses.InvateWriteMoneyString);
+
+            while (!(double.TryParse(getString(), out resoult)))
+            {
+                DisplayString?.Invoke(Resurses.ExeprionString);
+            }
+
+            return resoult;
+        }
+    }
+}
