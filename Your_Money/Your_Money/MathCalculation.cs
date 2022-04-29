22a3711dd84a7b7b0a27f96027a524e7ea60c8ef
Yaroslav131 <berserk12186@gmail.com>
2022-04-29 23:07:03 +0300

    Add MathCalculation class

---
+++ Your_Money/Your_Money/MathCalculation.cs
@@ -0,0 +1,18 @@
+﻿namespace Your_Money
+{
+    public class MathCalculation   
+    {
+        public double CalculateListSum<Q>(List<Q> entrys)
+            where Q : Entry
+        {
+            var sum = 0.0;
+
+            foreach (Q entry in entrys)
+            {
+                 sum += entry.Score;
+            }
+
+            return sum;
+        }
+    }
+}
