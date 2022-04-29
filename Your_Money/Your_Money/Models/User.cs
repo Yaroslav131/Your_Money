d9c83c0f348c11fc563ed155336c519b69f87b0a
Yaroslav131 <berserk12186@gmail.com>
2022-04-29 18:23:22 +0300

    Add user model

---
+++ Your_Money/Your_Money/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Your_Money.Models
+{
+    public class User
+    {
+        private UserControls controls;
+        private List<Income> incomes;
+        private List<Consumption> consumptions;
+        public double Arrive { get; set; }
+
+        public User()
+        {
+            Arrive = 0;
+            incomes = new List<Income>();
+            consumptions = new List<Consumption>();
+            controls = new UserControls();
+        }
+    }
+}
